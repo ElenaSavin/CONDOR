2aacfc86901f53bbbda48be5b0d771130ddbe8c1
Elena Savin <62284409+ElenaSavin@users.noreply.github.com>
2020-03-25 18:55:50 +0200

    download file from tcga

---
+++ download_fastq.py
@@ -0,0 +1,15 @@
+# access TCGA cloud credentials
+from os import read
+import sevenbridges as sbg
+import sys
+
+# cgc credentials
+api = sbg.Api(url='https://cgc-api.sbgenomics.com/v2', token='655963abb0424c3ca5bab03e16e0465c')
+
+# project details
+project_name = 'all_fastq'
+file_name = sys.argv[1]
+project_id = api.projects.query(name=project_name)
+file = api.files.query(project=project_id[0], names=[file_name])
+#file[0].download(path=file_name)
+print(file[0].id)
