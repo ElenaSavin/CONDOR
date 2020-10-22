977f56f8b78cc5b538f2339c59d97a4f21e23fe9
Elena Savin <62284409+ElenaSavin@users.noreply.github.com>
2020-10-22 14:08:55 +0300

    Update file_size.py

--- docker_image/file_size.py
+++ docker_image/file_size.py
@@ -1,10 +1,12 @@
-# access TCGA cloud credentials
+import os
 from os import read
 import sevenbridges as sbg
 import sys
 
 # cgc credentials
-api = sbg.Api(url='https://cgc-api.sbgenomics.com/v2', token='891b3a8c3eb2416685414a7b6e8e181b', advance_access=True)
+endpoint_url = os.environ['sb_endpoint_url']
+token = os.environ['sb_token']
+api = sbg.Api(url=endpoint_url, token=token, advance_access=True)
 
 # project details
 project_name = 'all_fastq'
@@ -12,10 +14,3 @@ fileid = sys.argv[1]
 file = api.files.get(id=fileid)
 
 print(file.size)
-
-
-#file_name = sys.argv[1]
-#project_id = api.projects.query(name=project_name)
-
-#file = api.files.query(project=project_id[0], names=[file_name])
-#file[0].download(path=file_name)
