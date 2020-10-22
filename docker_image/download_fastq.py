5e1daa043ddea216af9d491925e449528477c970
Elena Savin <62284409+ElenaSavin@users.noreply.github.com>
2020-10-22 14:04:48 +0300

    Update download_fastq.py

--- docker_image/download_fastq.py
+++ docker_image/download_fastq.py
@@ -1,15 +1,16 @@
-# access TCGA cloud credentials
 from os import read
+import os
 import sevenbridges as sbg
 import sys
 
 # cgc credentials
-api = sbg.Api(url='https://cgc-api.sbgenomics.com/v2', token='891b3a8c3eb2416685414a7b6e8e181b', advance_access=True)
+endpoint_url = os.environ['sb_endpoint_url']
+token = os.environ['sb_token']
+api = sbg.Api(url=endpoint_url, token=token, advance_access=True)
 
-# project details
-project_name = 'all_fastq'
+# project details - expecting your project name
+project_name = <name of the project on CGC>
 file_name = sys.argv[1]
 project_id = api.projects.query(name=project_name)
 file = api.files.query(project=project_id[0], names=[file_name])
 file[0].download(path=file_name)
-print(file[0].id)
