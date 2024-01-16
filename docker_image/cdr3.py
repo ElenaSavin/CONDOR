ce9714259c765efa675fc78f3374c460ec5575c6
lena <lena.savin22@gmail.om>
2024-01-16 14:51:40 +0200

    tmp

--- docker_image/cdr3.py
+++ docker_image/cdr3.py
@@ -1,8 +1,12 @@
-import Bio.SeqIO
-import hashlib
+import Bio.SeqIO 
 import csv
+import multiprocessing
+import logging
 
 def get_target_sequences(seq_file):
+  hashed_sequences = {}
+  base = 256
+  mod = 2**64
 # Open the CSV file in read mode
   with open(seq_file, "r") as csvfile:
     # Create a CSV reader object
@@ -11,21 +15,25 @@ def get_target_sequences(seq_file):
     # Read the header row
     header = next(csv_reader)
 
-    # Extract the first column values into a list
-    sequences = [row[0] for row in csv_reader]
-    frequencies = [row[1] for row in csv_reader]
-  # Print the first column values
-    hashed_sequences = {hashlib.sha256(seq.encode()).hexdigest(): seq for seq in sequences}
-  return(sequences, hashed_sequences)
+    for row in csv_reader:
+      sequence = row[0]
+      hashed_sequences[sequence] = initial_hash(sequence, base, mod)  # Use hashed sequence as key
+
+  return(hashed_sequences)
 
 def translate_frames(file_id):
   print(f"translating file id: {file_id}")
+  target_hashes = get_target_sequences("top_1000.csv")
+  
   for read in Bio.SeqIO.parse(file_id, "fastq"):
+    
     sequence = str(read.seq.complement())
+    #sequence = str(read.seq)
     for frame in range(3):
       translation = []
       for i in range(frame, len(sequence), 3):
         codon = sequence[i : i + 3]
+
         # Handle partial codons
         if len(codon) != 3:
           codon += "N" * (3 - len(codon))  # Add trailing Ns
@@ -33,27 +41,63 @@ def translate_frames(file_id):
         translation.append(amino_acid)
       
       translated_read = ("".join(translation))  # Join strings
+      # print(f"encoding and comparing read number: {read.id}, frame: {frame} read: {translated_read}")
+      # print(f"translated_read: {translated_read}")
+      with multiprocessing.Pool() as pool:
+        pool.starmap(process_target_sequence, [(target_sequence, sequence_hashed, translated_read, read, frame, logging.getLogger()) for target_sequence, sequence_hashed in target_hashes.items()])
+      #encode_compare(translated_read, read, frame)
 
-      encode_compare(translated_read, read, frame)
+def hash_read(translated_read, sequence_length):
+  hashed_read = {}
+  base = 256
+  mod = 2**64
+  
+  hashed_read[0] = initial_hash(translated_read[:sequence_length], base, mod)
+  for i in range(1, len(translated_read) - sequence_length + 1):
+    hashed_read[i] = rolling_hash(hashed_read[i-1], translated_read[i - 1], translated_read[i + sequence_length - 1], sequence_length, base)
+  return hashed_read
+  
+def rolling_hash(prev_hash, prev_char, next_char, sequence_length, base):
+  mod = 2**64  # Large prime for modulo operation to avoid overflow
+  new_hash = (prev_hash - ord(prev_char) * pow(base, sequence_length - 1, mod)) % mod
+  new_hash = (new_hash * base + ord(next_char)) % mod
+  return new_hash
 
+def initial_hash(s, base, mod):
+  h = 0
+  for char in s:
+      h = (h * base + ord(char)) % mod
+  return h
+
+def process_target_sequence(target_sequence, sequence_hashed, translated_read, read, frame, logger):
+  sequence_length = len(target_sequence)
+  for i, read_hash in hash_read(translated_read, sequence_length).items():   
+    if sequence_hashed == read_hash:
+      print(f"Target sequence found in read: {read.id}")
+      print(f"Frame: {frame + 1}")
+      print(f"Index: {i}")
+      print(f"Original sequence: {read.id}")
+      print(f"Original translated sequence: {translated_read}")
+
+#TODO algorithms instead of hash for parallel search
 def encode_compare(translated_read, read, frame):
-  target_sequences, target_hashes = get_target_sequences("top_1000.csv")
+  target_hashes = get_target_sequences("top_1000.csv")
+  #print(f"target sequences are: {target_hashes.keys()}")
+  for sequence, sequence_hashed in target_hashes.items():
+    sequence_length = len(sequence)
+    #print(f"searching for: {sequence}, length: {sequence_length}")
+    for i, read_hash in hash_read(translated_read, sequence_length).items():
+      
+      if sequence_hashed == read_hash: 
+        print(f"Target sequence found in read: {read.id}")
+        print(f"Frame: {frame + 1}")
+        print(f"index: {i}")
+        print(f"Original sequence: {read.id}")
+        print(f"Original translated sequence: {translated_read}")
 
-  for substring in target_sequences:
-    substring_length = len(substring)
-    for i in range(len(translated_read) - substring_length + 1):
-      read_hash = hashlib.sha256(translated_read[i:i+substring_length].encode()).hexdigest()
-      # Check if any target hash is a substring of the read hash
-      for target_hash in target_hashes:
-        if target_hash in read_hash: 
-          print(f"Target sequence found in read: {read.id}")
-          print(f"Frame: {frame + 1}")
-          print(f"Original sequence: {read.id}")
-          print(f"Original translated sequence: {translated_read}")
-
-          # Find the specific matching target sequence
-          matching_target = target_hashes.get(read_hash)
-          if matching_target:  # Exact match
-            print(f"Matching target sequence: {matching_target}")
-          else:  # Partial match
-            print(f"Partial match with a target sequence")
+        # # Find the specific matching target sequence
+        # matching_target = target_hashes.values().get(read_hash)
+        # if matching_target:  # Exact match
+        #   print(f"Matching target sequence: {matching_target}")
+        # else:  # Partial match
+        #   print(f"Partial match with a target sequence")
--- docker_image/main.py
+++ docker_image/main.py
@@ -7,6 +7,7 @@ from cdr3 import translate_frames, get_target_sequences
 
 #token to secured data 
 token_file = "token.txt"
+project = "brca"
 
 def download(data_endpt, token_string, file_id):
   params = {"gencode": ["BRCA1", "BRCA2"]}
@@ -17,7 +18,7 @@ def download(data_endpt, token_string, file_id):
                             "X-Auth-Token": token_string
                             })
 
-  file_name = f"files/brca_{file_id}.bam"
+  file_name = f"files/{project}_{file_id}.bam"
   
   # Create the "files" directory if it doesn't exist
   os.makedirs("files", exist_ok=True)  # Create directory only if needed
@@ -25,24 +26,28 @@ def download(data_endpt, token_string, file_id):
   with open(file_name, "wb") as output_file:
     output_file.write(response.content)
       
-  system(f"samtools bam2fq files/brca_{file_id}.bam > files/brca_{file_id}.fastq")
+  system(f"samtools bam2fq files/{project}_{file_id}.bam > files/{project}_{file_id}.fastq")
   
 #read the file ids from file
 def match(manifest):
+  start_time = time.time()
+  print(f"Processing started at: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
   with open(manifest, "r") as file:
     for line in file:
       file_id = line.strip() 
       data_endpt = f"https://api.gdc.cancer.gov/slicing/view/{file_id}"
+      
       try: 
-        open(f"files/brca_{file_id}.fastq")
+        open(f"files/{project}_{file_id}.fastq")
         print(f"File: {file_id} exists, Skipping Download")
+        translate_frames(f"files/{project}_{file_id}.fastq")
       except FileNotFoundError:
         print(f"Downloading file id: {file_id}")
         with open(token_file,"r") as token:
           token_string = str(token.read().strip())
         try:
           download(data_endpt, token_string, file_id)
-          translate_frames(f"files/brca_{file_id}.fastq")
+          translate_frames(f"files/{project}_{file_id}.fastq")
         except requests.exceptions.ConnectionError as e:
           if "Temporary failure in name resolution" in str(e):
             print(f"Temporary DNS resolution error. File {file_id} was not downloaded")
@@ -50,12 +55,17 @@ def match(manifest):
           # Code to retry the operation (e.g., call the function again)
           else:
             print("A different connection error occurred:", e)
-             
-        try:
-          remove(f"files/brca_{file_id}.bam")
-        except FileNotFoundError:
-          print("File not found.")
+  end_time = time.time()  # Record end time
+  print(f"Processing finished at: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
+  print(f"Total processing time: {end_time - start_time:.2f} seconds")
+           
+        # try:
+        #   remove(f"files/{project}_{file_id}.bam")
+        # except FileNotFoundError:
+        #   print("File not found.")
  
+#TODO add logger
 if __name__ == "__main__":
     manifest = "brca.txt"  # Replace with your actual FASTQ file name
     match(manifest)
+    
